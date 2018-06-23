aa37ceae0b7f73ec6ce78934718293ab8b1b6b9a
ZayAlexNick <zay.alex.nick@gmail.com>
2018-06-23 07:45:44 +0700

    theme

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import configureStore from "~/redux/configureStore";
+import { ThemeProvider } from 'styled-components';
 
+import configureStore from "~/redux/configureStore";
+import '~/theme/baseStyles';
 import Root from '~/scenes/Root';
+import theme from '~/theme';
 
 export default () => (
     <Provider store={configureStore()}>
-        <Root />
+        <ThemeProvider theme={theme}>
+            <Root />
+        </ThemeProvider>
     </Provider>
 );
---
+++ src/theme/baseStyles.js
@@ -0,0 +1,13 @@
+import { injectGlobal } from 'styled-components';
+
+injectGlobal`
+  * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+  
+  body {
+    font: 16px/1 'Comfortaa', Helvetica, Arial, sans-serif;
+  }
+`;
